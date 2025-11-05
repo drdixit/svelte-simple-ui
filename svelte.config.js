aa9c5036caff85d6fe5e4b384eb271007bba7e36
Dixitkumar Bhuva <100849131+dixitbhuva@users.noreply.github.com>
2025-11-05 10:01:03 +0530

    Switch to Cloudflare adapter and update preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
-// Use the static adapter for a production-ready static site
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	preprocess: preprocess(),
-	kit: {
-		adapter: adapter({
-			// default options fine; output will be in build/
-			pages: 'build',
-			assets: 'build'
-		}),
-		// prerendering will be handled during the static build step
-	}
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
